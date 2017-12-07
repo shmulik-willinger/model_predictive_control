ff8d2933cb96a5e78e4a73bdd8843af4e8cb1497
ShmuliShmulik <shmulik83@gmail.com>
2017-12-07 23:44:33 +0200

    init FG_eval class

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 10;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -15,17 +15,31 @@ double dt = 0;
 // simulator around in a circle with a constant steering angle and velocity on a
 // flat terrain.
 //
-// Lf was tuned until the the radius formed by the simulating the model
+// Lf was tuned until the radius formed by the simulating the model
 // presented in the classroom matched the previous radius.
 //
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
 
-class FG_eval {
+double ref_v = 80;
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
+class FG_eval 
+{
  public:
   // Fitted polynomial coefficients
   Eigen::VectorXd coeffs;
-  FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
+  FG_eval(Eigen::VectorXd coeffs) 
+  { 
+	  this->coeffs = coeffs; 
+  }
 
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
   void operator()(ADvector& fg, const ADvector& vars) {
